19fb55c568fb3164bce9b2e87cfd39601739f95e
mishelsingh <105253533+mishelsingh@users.noreply.github.com>
2023-04-18 15:36:45 +0100

    Direction page RD Map
    added the map to the direction page. changed few css around the html

---
+++ html/script.js
@@ -0,0 +1,31 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJua3BpcGVib21iIiwiYSI6ImNsZnI2a3ZodTA1Z2kzemtpcHd3Ymcza2gifQ.c0-7l0K5Wu57VINpenRkTQ';
+
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {enableHighAccuracy: true})
+
+function successLocation(position){
+    console.log(position)
+    setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation(){
+    setupMap([-2.5918, 51.4538])
+}
+
+function setupMap(center) {
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: 'mapbox://styles/mapbox/streets-v12', // style URL
+        center: center, // starting position [lng, lat]
+        zoom: 15 // starting zoom
+        });
+
+    const nav = new mapboxgl.NavigationControl()
+    map.addControl(nav)
+
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    })
+
+    map.addControl(directions, "top-left")
+}
